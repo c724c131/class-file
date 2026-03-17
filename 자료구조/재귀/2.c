d67ebc7cad2d5604f8167decbacffb1f43ac6274
c724c131 <c724c131@gmail.com>
2026-03-17 16:02:09 +0900

    Update 2.c

--- 자료구조/재귀/2.c
+++ 자료구조/재귀/2.c
@@ -1,1 +1,20 @@
 /* 양의 정수를 입력 받아, 각 자리의 수를 높은 자릿수부터 차례로 출력하는 프로그램을 작성하시오*/
+#include <stdio.h>
+
+int sum(int n) {
+	if (n <= 1){
+		return 1;
+	}
+	else if(n>=2) {
+		return n + sum(n - 1);
+	};
+}
+
+int main() {
+	int result;
+	int n;
+	scanf_s("%d", &n);
+	result = sum(n);
+	printf("%d", result);
+	return 0;
+}
