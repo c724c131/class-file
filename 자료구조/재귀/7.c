b9a7cb160f9c0806264e8802893e6ff2a3389b6b
c724c131 <c724c131@gmail.com>
2026-03-17 17:47:55 +0900

    Update 7.c

--- 자료구조/재귀/7.c
+++ 자료구조/재귀/7.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int rCountChar(char str[], char c, int i) {
+
+	int cnt = (str[i] == c) ? 1 : 0;
+
+	if (i == 0)
+		return cnt;
+	else
+		return cnt + rCountChar(str, c, i - 1);
+}
+
+int main() {
+	char str[100], c;
+	scanf_s("%s", str, (int)sizeof(str));
+	scanf_s(" %c", &c, sizeof(c));
+
+	printf("%d\n", rCountChar(str, c, sizeof(c)));
+}
