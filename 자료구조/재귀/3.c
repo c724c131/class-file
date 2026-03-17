a5445851ea84a7e5c2b9f4be9eed518a3929a853
c724c131 <c724c131@gmail.com>
2026-03-17 16:37:12 +0900

    Update 3.c

--- 자료구조/재귀/3.c
+++ 자료구조/재귀/3.c
@@ -1,1 +1,20 @@
 /*양의 정수를 입력 받아, 각 자리의 수를 낮은 자릿수부터 차례로 출력하는 프로그램을 작성하시오.*/
+#include <stdio.h>
+#include <math.h>
+
+void high(int n) {
+	if (n < 10) {
+		printf("%d ", n);
+		return;
+	}
+
+	printf("%d\n", n % 10);
+	return high(n / 10);
+}
+
+int main() {
+	int n;
+	scanf_s("%d", &n);
+	high(n);
+	return 0;
+}
