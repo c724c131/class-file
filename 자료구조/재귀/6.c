b03a2354aa19993edfbed00e822a4ac5878caed7
c724c131 <c724c131@gmail.com>
2026-03-17 17:32:44 +0900

    Update 6.c

--- 자료구조/재귀/6.c
+++ 자료구조/재귀/6.c
@@ -1,1 +1,21 @@
 /*유클리드 호제법*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* a >= b가 조건*/
+
+int gcd(int a, int b) {
+	if (b == 0)
+		return a;
+	else
+		return gdc(b, a % b);
+}
+
+int main() {
+	int a;
+	int b;
+	scanf_s("%d %d", &a, &b);
+	
+	return 0;
+}
