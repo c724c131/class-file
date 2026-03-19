66ae6323dc18fa306777b5f352c37b474405b874
c724c131 <c724c131@gmail.com>
2026-03-19 10:01:01 +0900

    Update polynomialAdd.c

--- 자료구조/다항식/polynomialAdd.c
+++ 자료구조/다항식/polynomialAdd.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+#define N 10 ///최고 차항은 9차
+
+typedef struct polynomial {
+	int degree;
+	int coef[N];
+}Poly;
+
+Poly add(Poly a, Poly b) {
+	Poly c;
+	c.degree = (a.degree > b.degree) ? a.degree : b.degree;
+
+	for (int i = 0;i <= c.degree;i++) {
+		c.coef[i] = ((i <= a.degree) ? a.coef[i] : 0) + 
+			        ((i <= b.degree) ? b.coef[i] : 0);
+	}
+	return c;
+}
+
+void print(Poly p, const char* str) {
+	printf("%s", str);
+	for (int i = p.degree;i > 0;i--) {
+		printf("%2dx^%d +", p.coef[i], i);
+	}
+	printf("%2d\n", p.coef[0]);
+}
+
+int main() {
+	Poly a, b ,result;
+	printf("차수를 입력하세요(a b) : ");
+	scanf_s("%d %d", &a.degree, &b.degree);
+
+	for (int i = a.degree;i >= 0;i--)///뒤에서부터 대입
+	{
+		scanf_s("%d", &a.coef[i]);
+	}
+	for (int i = b.degree;i >= 0;i--)
+	{
+		scanf_s("%d", &b.coef[i]);
+	}
+	print(a, "A =");
+	print(b, "B =");
+
+	result = add(a, b);
+
+	print(result, "result =");
+
+	return 0;
+}
