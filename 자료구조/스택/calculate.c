83d3d0ec3e6cd5c0cc47ddc6bea77436bf96085a
c724c131 <c724c131@gmail.com>
2026-03-26 10:16:06 +0900

    Update calculate.c

--- 자료구조/스택/calculate.c
+++ 자료구조/스택/calculate.c
@@ -1,1 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define N 50
 
+typedef int element; /*이렇게 해놓으면 나중에 스택 종류를 쉽게 바꿀 수 있다.*/ /*스택 안에서만 써야함*/
+
+typedef struct {
+	element stack[N];
+	int top;
+}StackType;
+
+void init(StackType* S) {/*주소는 포인터 변수로 받는다 (정의할때는 *S 넣는값은 주소)*/
+	S->top = -1;
+};
+
+void print(StackType* S) {
+	for (int i = S->top; i >= 0;i--) {
+		printf("|   %c   |\n", S->stack[i]);
+		printf("---------\n");
+	}
+};
+
+int isEmpty(StackType* S) {
+	return S->top == -1;
+};
+
+int isFull(StackType* S) {
+	return S->top == N - 1;
+};
+
+void push(StackType* S, element e) {
+	if (isFull(S))
+		printf("Overflow\n");
+	else {
+		S->top++;
+		S->stack[S->top] = e;
+	}
+};
+
+element pop(StackType* S) {
+	if (isEmpty(S)) {
+		printf("Empty!\n");
+		return -1;
+	}
+	element e = S->stack[S->top];
+	S->top--;
+	return e;
+}
+
+element peek(StackType* S) {
+	if (isEmpty(S)) {
+		printf("Empty!\n");
+		return -1;
+	}
+	return S->stack[S->top];
+}
+
+int evaluate(char str[]) {///일단은 한자리 연산 숙제 : 여러자리 연산 만들기 (후위연산)
+	StackType S;
+	init(&S);
+
+	int op1, op2, num;
+	int n = strlen(str);
+	char c;
+
+	for (int i = 0;i < n;i++) {
+		c = str[i];
+		if (isdigit(c)) {
+			num = c - '0';
+			push(&S, num);
+		}
+		else if (strchr("+-*/", c)) {
+			op2 = pop(&S);
+			op1 = pop(&S);
+
+			switch (c) {
+				case '+':push(&S, op1 + op2);break;
+				case '-':push(&S, op1 - op2);break;
+				case '*':push(&S, op1 * op2);break;
+				case '/':push(&S, op1 / op2);break;
+			}
+		}
+	}
+	return pop(&S);
+};
+
+int main() {
+	char str[N];
+	printf("Input : ");
+	fgets(str, N, stdin);
+
+	printf("%d", evaluate(str));
+	return 0;
+}
+
+///전역변수로 스텍을 만들면 한 개밖에 못씀 그리고 전역변수로 중요한 데이터를 다루는 건 좋지 않음
