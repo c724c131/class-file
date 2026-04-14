d6812584bfdcb498b76ba3e32d9276ede7663ba2
c724c131 <c724c131@gmail.com>
2026-04-14 14:56:03 +0900

    Update List.c

--- 자료구조/list/List.c
+++ 자료구조/list/List.c
@@ -3,133 +3,122 @@
 
 typedef char element;
 
-typedef struct Node {
+typedef struct Node {//노드 하나
 	element data;
+	struct Node* prev;//이전 노드
 	struct Node* next;//자기 참조 구조체 : Node 안에서 Node 참조
 }Node; //자기 참조를 하려면 구조체 테그를 생략하면 안된다.
 
-typedef struct {
-	Node* head;
+typedef struct {//이중 연결 리스트
+	Node* head; //헤드 노드
+	Node* tail; //테일 노드
 	int size;
-}ListType;
+}DListType;
 
-void init(ListType* L) {
-	L->head = NULL; //포인터 변수는 NULL로 초기화
-	L->size = 0;
-};
-
-int isEmpty(ListType* L) {//isFull은 필요없지만 이건 필요
-	return L->size == 0;
-}
-
-Node* makeNode(element e) {//메모리 할당시 받는 주소를 이용
-	Node* node = (Node*)malloc(sizeof(Node));//엄밀히 하려면 여기서 NULL 체크
+Node* makeNode(element e) {//이중 연결 리스트는 init 이전에 노드가 있어야 한다.
+	Node* node = (Node*)malloc(sizeof(Node));
 	node->data = e;
-	node->next = NULL;
+	node->prev = node->next = NULL;
 
 	return node;
 }
 
-//head 유치원 선생님같은것
-void insertFirst(ListType* L, element e) {
-	Node* node = makeNode(e);
-	node->next = L->head; //선생님이 잡고있던 애를 잡기
-	L->head = node; //선생님은 방금 들어온 애 잡기
-	L->size++;
-}
+void init(DListType* DL) {
+	DL->head = makeNode(0);//아무값이나
+	DL->tail = makeNode(0);
 
-element deleteFirst(ListType* L) {
-	if (isEmpty(L)) {
-		printf("Empty\n");
-		return '\0'; //또는 0 또는 exit
-	}
-
-	Node* p = L->head;
-	element data = p->data;
-	L->head = p->next; //이제 p는 없어도 된다
-	free(p);
-	L->size--;
-
-	return data;
-}
-
-void insert(ListType* L, element e, int pos) {
-	if (pos <= 0 || pos > L->size + 1) {
-		printf("Invaild Position");
-		return 0;
-	}
-
-		if (pos == 1)
-			insertFirst(L, e);
-		else {
-			Node* node = makeNode(e);
-			Node* p = L->head; //가야할 포지션 값의 -2 만큼만 움직여야한다.
-
-			for (int i = 2;i < pos;i++) {
-				p = p->next;
-			}
-			node->next = p->next;
-			p->next = node;
-			L->size++;
-		}
-}
-
-void print(ListType* L) {
-	Node* p = L->head;
-
-	while (p != NULL) {
-		printf("[%c]", p->data);
-		if (p->next != NULL)
-			printf("->"); //보기 좋은 화살표
-		p = p->next; //포인터 이동
-	}
-	printf("\n");
+	DL->head->next = DL->tail;
+	DL->tail->prev = DL->head;
+	DL->size = 0;
 };
 
-element deletePosition(ListType* L, int pos) {
-	if (isEmpty(L)) {
-		printf("Empty\n");
-		return '\0';
+int isEmpty(DListType* DL) {//isFull은 필요없지만 이건 필요
+	return DL->size == 0;
+}
+
+void insertFirst(DListType* DL, element e) {
+	Node* p = DL->head;
+	Node* node = makeNode(e);
+
+	node->prev = p;
+	node->next = p->next;
+	p->next->prev = node;
+	p->next = node;
+
+	DL->size++;
+}
+
+void insert(DListType* DL, element e, int pos) {
+	if (pos <= 0 || pos > DL->size + 1) {
+		printf("Invaild Position.\n");
+		return;
 	}
 
-	if (pos <= 0 || pos > L->size + 1) {
-		printf("Invaild Position");
-		return '\0';
-	}
-	if (pos == 1)
-		deleteFirst(L);
-
-	Node* p = L->head;
-	for (int i = 2;i < pos;i++) {
+	Node* p = DL->head;
+	for (int i = 1; i < pos; i++) {//가야할 포지션 값의 -1 만큼만 움직인다.
 		p = p->next;
 	}
 
-	Node* temp = p->next;
-	element data = temp->data;
-	p->next = temp->next;
-	free(temp);
-	L->size--;
+	Node* node = makeNode(e);
+
+	node->prev = p;
+	node->next = p->next;
+	p->next->prev = node;
+	p->next = node;
+
+	DL->size++;
+}
+
+element delete(DListType* DL, int pos) {
+	if (isEmpty(DL)) {
+		printf("Empty!.\n");
+		return 0;
+	}
+
+	if (pos <= 0 || pos > DL->size + 1) {
+		printf("Invaild Position.\n");
+		return 0;
+	}
+
+	Node* p = DL->head->next;
+	for (int i = 1; i < pos; i++) {
+		p = p->next;
+	}
+
+	element data = p->data;
+
+	p->prev->next = p->next;
+	p->next->prev = p->prev;
+	free(p);
+	DL->size--;
 
 	return data;
 }
 
+void print(DListType* DL) {
+	for (Node* p = DL->head->next; p != DL->tail; p = p->next) {
+		printf("[%c]", p->data);
+		if (p->next != DL->tail)
+			printf("<->");
+	}
+	printf("\n");
+}
+
 int main(void) {
-	ListType L;
-	init(&L);
+	DListType DL;
+	init(&DL);
 
-	insertFirst(&L,'A');
-	insertFirst(&L,'B');
-	insertFirst(&L,'C');
-	insertFirst(&L, 'D');
+	insert(&DL, 'A', 1); print(&DL);
+	insert(&DL, 'B', 2); print(&DL);
+	insert(&DL, 'C', 3); print(&DL);
+	insert(&DL, 'D', 4); print(&DL);
+	insert(&DL, 'E', 5); print(&DL);
+	insert(&DL, 'F', 6); print(&DL);
+	insertFirst(&DL, 'K'); print(&DL);
 
-	insert(&L, 'I', 4);
-	insert(&L, 'K', 3);
-
-	print(&L);
-	printf("Del : [%c]\n", deleteFirst(&L));
-	print(&L);
-	printf("Del : [%c]\n", deletePosition(&L, 2));
-	print(&L);
+	delete(&DL, 3); print(&DL);
 
 	return 0;
 }
+
+//자료구조 책에 있는거 구현 리버스나 다른거 등등
