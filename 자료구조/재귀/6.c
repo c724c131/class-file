f28d40f7cdf260f0e705fb16d6e3745922221f23
c724c131 <c724c131@gmail.com>
2026-04-23 02:47:14 +0900

    Update 6.c

--- 자료구조/재귀/6.c
+++ 자료구조/재귀/6.c
@@ -9,13 +9,13 @@ int gcd(int a, int b) {
 	if (b == 0)
 		return a;
 	else
-		return gdc(b, a % b);
+		return gcd(b, a% b);
 }
 
 int main() {
 	int a;
 	int b;
 	scanf_s("%d %d", &a, &b);
-	
+	printf("%d", gcd(a, b));
 	return 0;
 }
